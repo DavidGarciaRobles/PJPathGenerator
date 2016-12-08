3af27c41789393b8477c6db8478f7063add69fbf
Diego' <Diego'@DIVER-PC>
2016-12-08 18:40:57 +0100

    Se incluye angular-translate  y ficheros con traducciones.

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-﻿angular.module('myApp', []).config(function($logProvider) {
+﻿var app = angular.module('myApp', ['pascalprecht.translate']).config(function($logProvider) {
     // habilitado logger debug para trazas de prueba
   $logProvider.debugEnabled(true);
+});
+app.config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
+app.config(function ($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        files: [{
+            prefix: '/resources/i18n/locale-',
+            suffix: '.json'
+        }]
+    });
+    $translateProvider.preferredLanguage('es_ES');
 });
