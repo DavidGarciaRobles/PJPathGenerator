1699eae541ef9ad21091eb619df8fddd2618b693
XellosMetallium <David.garcia.robles@gmail.com>
2016-12-08 18:40:58 +0100

    Añadida carga de combos con angular

--- controllers/CharacterGeneratorController.js
+++ controllers/CharacterGeneratorController.js
@@ -1,8 +1,26 @@
-﻿angular.module("myApp").controller('CharacterGeneratorController', ['$scope', '$log', function ($scope, $log) {
-    $log.debug('Hello Debug!');
+﻿var app = angular.module("myApp",[]);
 
-    // Generacion de NPC
-    $scope.generateNPC = function () {
-        $log.info('CharacterGeneratorController - starting npc generating!');
-    }
+app.controller('CharacterGeneratorController', ['$scope', '$log', function ($scope, $log)
+{
+    $log.debug('Hello Debug!');
 }]);
+
+app.controller("CargaComboClases", function ($scope, $http)
+{
+    //vamos a hacer uso de $http para obtener los datos
+    $http.get('http://localhost:51348/../ClasesNPC.json').success(function (data)
+    {
+        //enviamos los datos a la vista con el objeto $scope
+        $scope.datos = data;
+    });
+})
+
+app.controller("CargaComboRazas", function ($scope, $http)
+{
+    //vamos a hacer uso de $http para obtener los datos
+    $http.get('http://localhost:51348/../Razas.json').success(function (data)
+    {
+        //enviamos los datos a la vista con el objeto $scope
+        $scope.datos = data;
+    });
+})
